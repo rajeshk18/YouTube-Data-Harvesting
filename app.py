6a8c5b111544b27d480251a703cb2230536e0945
rajeshk18 <128392370+rajeshk18@users.noreply.github.com>
2023-07-25 22:56:49 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb+srv://arajeshkanna82:r5HSCqyWVxkSQukW@youtubedb.weh8pk8.mongodb.net/?retryWrites=true&w=majority")
+mydb = myclient["youtubedatabase"]
+mycol = mydb["Channel_Name"]
+
+mydict = {
+        "Channel_Name": "Example Channel",
+        "Channel_Id": "UC1234567890",
+        "Subscription_Count": 10000,
+        "Channel_Views": 1000000,
+        "Channel_Description": "This is an example channel.",
+        "Playlist_Id": "PL1234567890"
+}
+
+x = mycol.insert_one(mydict)
+
+print(x.inserted_id)
