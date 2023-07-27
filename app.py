e0307adf8a877876c1771d704e2d8a02330546e1
rajeshk18 <128392370+rajeshk18@users.noreply.github.com>
2023-07-27 19:13:13 +0530

    connecting Youtube

--- app.py
+++ app.py
@@ -6,14 +6,30 @@ import pandas as pd
 import pydeck as pdk
 import streamlit as st
 import pymongo
+import plotly.express as px
+from streamlit_option_menu import option_menu
+from googleapiclient.discovery import build
+from PIL import Image
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
-st.set_page_config(layout="wide", page_title="NYC Ridesharing Demo", page_icon=":taxi:")
+st.set_page_config(layout="wide", page_title="Youtube Harvesting | By Rajesh", page_icon=":youtube:", menu_items={'About': """# Demo Project *"""})
 
+# CREATING OPTION MENU
+with st.sidebar:
+    selected = option_menu(None, ["Home","Extract and Transform","View"], 
+                           icons=["house-door-fill","tools","card-text"],
+                           default_index=0,
+                           orientation="vertical",
+                           styles={"nav-link": {"font-size": "30px", "text-align": "centre", "margin": "0px", 
+                                                "--hover-color": "#C80101"},
+                                   "icon": {"font-size": "30px"},
+                                   "container" : {"max-width": "6000px"},
+                                   "nav-link-selected": {"background-color": "#C80101"}})
+        
+# MongoDB connection
 myclient = pymongo.MongoClient("mongodb+srv://arajeshkanna82:r5HSCqyWVxkSQukW@youtubedb.weh8pk8.mongodb.net/?retryWrites=true&w=majority")
 mydb = myclient["youtubedatabase"]
 mycol = mydb["Channel_Name"]
-
 mydict = {
         "Channel_Name": "Example Channel",
         "Channel_Id": "UC1234567890",
@@ -23,6 +39,34 @@ mydict = {
         "Playlist_Id": "PL1234567890"
 }
 
-x = mycol.insert_one(mydict)
+# x = mycol.insert_one(mydict)
+# print(x.inserted_id)
 
-print(x.inserted_id)
+# BUILDING CONNECTION WITH YOUTUBE API
+# api_key = "AIzaSyCkglXpsoXo7QjsLDBAL8mzCfX4YZzpdtg"
+api_key = "AIzaSyAO0xwroUQypqQyPpmsKACcv-q4O3Km0sk"
+youtube = build('youtube','v3',developerKey=api_key)
+
+# GET CHANNEL DETAILS
+def get_channel_details(channel_id):
+    ch_data = []
+    response = youtube.channels().list(part = 'snippet,contentDetails,statistics', id= channel_id).execute()
+
+    for i in range(len(response['items'])):
+        data = dict(Channel_id = channel_id[i],
+                    Channel_name = response['items'][i]['snippet']['title'],
+                    Playlist_id = response['items'][i]['contentDetails']['relatedPlaylists']['uploads'],
+                    Subscribers = response['items'][i]['statistics']['subscriberCount'],
+                    Views = response['items'][i]['statistics']['viewCount'],
+                    Total_videos = response['items'][i]['statistics']['videoCount'],
+                    Description = response['items'][i]['snippet']['description'],
+                    Country = response['items'][i]['snippet'].get('country')
+                    )
+        ch_data.append(data)
+    return ch_data
+
+st.markdown("#    ")
+st.write("### Enter YouTube Channel_ID below :")
+ch_id = st.text_input("Hint : Goto channel's home page > Right click > View page source > Find channel_id").split(',')
+ch_details = get_channel_details(ch_id)
+print(ch_details)
