53ad3597483049b425921bf9c21a4be9d16aa4f3
rajeshk18 <128392370+rajeshk18@users.noreply.github.com>
2023-07-31 20:04:58 +0530

    all operations started

--- app.py
+++ app.py
@@ -70,6 +70,94 @@ def get_channel_detail(channel_id):
         ch_data.append(data)
     return ch_data
 
+# FUNCTION TO GET VIDEO IDS
+def get_channel_videos(channel_id):
+    video_ids = []
+    # get Uploads playlist id
+    res = youtube.channels().list(id=channel_id, 
+                                  part='contentDetails').execute()
+    playlist_id = res['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+    next_page_token = None
+    
+    while True:
+        res = youtube.playlistItems().list(playlistId=playlist_id, 
+                                           part='snippet', 
+                                           maxResults=50,
+                                           pageToken=next_page_token).execute()
+        
+        for i in range(len(res['items'])):
+            video_ids.append(res['items'][i]['snippet']['resourceId']['videoId'])
+        next_page_token = res.get('nextPageToken')
+        
+        if next_page_token is None:
+            break
+    return video_ids
+
+
+# FUNCTION TO GET VIDEO DETAILS
+def get_video_details(v_ids):
+    video_stats = []
+    
+    for i in range(0, len(v_ids), 50):
+        response = youtube.videos().list(
+                    part="snippet,contentDetails,statistics",
+                    id=','.join(v_ids[i:i+50])).execute()
+        for video in response['items']:
+            video_details = dict(Channel_name = video['snippet']['channelTitle'],
+                                Channel_id = video['snippet']['channelId'],
+                                Video_id = video['id'],
+                                Title = video['snippet']['title'],
+                                Tags = video['snippet'].get('tags'),
+                                Thumbnail = video['snippet']['thumbnails']['default']['url'],
+                                Description = video['snippet']['description'],
+                                Published_date = video['snippet']['publishedAt'],
+                                Duration = video['contentDetails']['duration'],
+                                Views = video['statistics']['viewCount'],
+                                Likes = video['statistics'].get('likeCount'),
+                                Comments = video['statistics'].get('commentCount'),
+                                Favorite_count = video['statistics']['favoriteCount'],
+                                Definition = video['contentDetails']['definition'],
+                                Caption_status = video['contentDetails']['caption']
+                               )
+            video_stats.append(video_details)
+    return video_stats
+
+
+# FUNCTION TO GET COMMENT DETAILS
+def get_comments_details(v_id):
+    comment_data = []
+    try:
+        next_page_token = None
+        while True:
+            response = youtube.commentThreads().list(part="snippet,replies",
+                                                    videoId=v_id,
+                                                    maxResults=100,
+                                                    pageToken=next_page_token).execute()
+            for cmt in response['items']:
+                data = dict(Comment_id = cmt['id'],
+                            Video_id = cmt['snippet']['videoId'],
+                            Comment_text = cmt['snippet']['topLevelComment']['snippet']['textDisplay'],
+                            Comment_author = cmt['snippet']['topLevelComment']['snippet']['authorDisplayName'],
+                            Comment_posted_date = cmt['snippet']['topLevelComment']['snippet']['publishedAt'],
+                            Like_count = cmt['snippet']['topLevelComment']['snippet']['likeCount'],
+                            Reply_count = cmt['snippet']['totalReplyCount']
+                           )
+                comment_data.append(data)
+            next_page_token = response.get('nextPageToken')
+            if next_page_token is None:
+                break
+    except:
+        pass
+    return comment_data
+
+
+# FUNCTION TO GET CHANNEL NAMES FROM MONGODB
+def channel_names():   
+    ch_name = []
+    for i in db.channel_details.find():
+        ch_name.append(i['Channel_name'])
+    return ch_name
+        
 if selected == "Home":
     col1,col2 = st.columns(2,gap= 'medium')
     col1.markdown("## :blue[Domain] : Social Media")
