8f8953652a4dca8b4aa297b6ada236d6a862338b
rajeshk18 <128392370+rajeshk18@users.noreply.github.com>
2023-07-31 16:03:37 +0530

    comments corrected

--- app.py
+++ app.py
@@ -75,12 +75,7 @@ if selected == "Home":
     col1,col2 = st.columns(2,gap= 'medium')
     col1.markdown("## :blue[Domain] : Social Media")
     col1.markdown("## :blue[Technologies] : Python, Streamlit, MongoDB, Youtube API, Microsoft Sql Server, ")
-    col1.markdown("## :blue[Goal] : A Streamlit application that allows users to access and analyze data from multiple YouTube channels. The application should have the following features:
-  Ability to input a YouTube channel ID and retrieve all the relevant data (Channel name, subscribers, total video count, playlist ID, video ID, likes, dislikes, comments of each video) using Google API.
- Option to store the data in a MongoDB database as a data lake.
- Ability to collect data for up to 10 different YouTube channels and store them in the data lake by clicking a button.
- Option to select a channel name and migrate its data from the data lake to a SQL database as tables.
-Ability to search and retrieve data from the SQL database using different search options, including joining tables to get channel details."")
+    col1.markdown("## :blue[Goal] : A Streamlit application that allows users to access and analyze data from multiple YouTube channels. The application should have the following features:   Ability to input a YouTube channel ID and retrieve all the relevant data (Channel name, subscribers, total video count, playlist ID, video ID, likes, dislikes, comments of each video) using Google API. Option to store the data in a MongoDB database as a data lake. Ability to collect data for up to 10 different YouTube channels and store them in the data lake by clicking a button.  Option to select a channel name and migrate its data from the data lake to a SQL database as tables. Ability to search and retrieve data from the SQL database using different search options, including joining tables to get channel details.")
     col2.markdown("#   ")
     col2.markdown("#   ")
     col2.markdown("#   ")
